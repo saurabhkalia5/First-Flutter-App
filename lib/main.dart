9b7b85ea4c1c1dbd31e1b7b38882c6bb67759794
Saurabh Kalia <105939712+saurabhkalia5@users.noreply.github.com>
2023-03-02 10:58:36 +0530

    int data type declared
    first data declaration

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,20 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MyApp());
-  runApp(MyWidget());
 }
 
-class MyApp
-  extends StatelessWidget {
-  const MyApp
-   ({super.key});
+class MyApp extends StatelessWidget {
+ 
 
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+
     return MaterialApp(
       home: Material(
         child: Center(
           child: Container(
-            child: Text("30 days of flutter"),
+            child: Text("Hello Brahm $days"),
           ),
         ),
       )
