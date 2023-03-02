ff315ecf2e79d1e9575312aa0a17fd3a04a8633e
Saurabh Kalia <105939712+saurabhkalia5@users.noreply.github.com>
2023-03-02 10:37:07 +0530

    First COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+  runApp(MyWidget());
+}
+
+class MyApp
+  extends StatelessWidget {
+  const MyApp
+   ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("30 days of flutter"),
+          ),
+        ),
+      )
+    );
+  }
+}
+
