04b4a23bc1c1d30c0a7d3164243ace969f867b92
Saurabh Kalia <105939712+saurabhkalia5@users.noreply.github.com>
2023-03-02 12:54:19 +0530

    Routes Integrated
    Routes for login and homepage have been integrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/homepage.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
 
 void main(){
   runApp(MyApp());
@@ -12,16 +13,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return  MaterialApp(
-      home: HomePage(),
-      themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
+      themeMode: ThemeMode.light,
+      // darkTheme: ThemeData(
+      //   brightness: Brightness.dark,
+      // ),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
       ),
+      routes: {
+        "/":(context) => Login(),
+        "/login":(context) => Login(),
+        "/home":(context) => HomePage()
+      },
     );
   }
 
     bringVeggies({required bool thaila , int rupees= 100}){
-
+      //{ } isbracket k adnar wale sare variables are optional params to be passed  
     }    
 
 }
