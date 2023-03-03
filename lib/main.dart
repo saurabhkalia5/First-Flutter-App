201c20544060f4b1ad5fb2a326567388ee65f9f3
Saurabh Kalia <105939712+saurabhkalia5@users.noreply.github.com>
2023-03-03 11:30:04 +0530

    Login Navigation Introduced
    Navigation.pushName

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/homepage.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main(){
@@ -24,8 +25,8 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         "/":(context) => Login(),
-        "/login":(context) => Login(),
-        "/home":(context) => HomePage()
+        MyRoutes.loginRoute :(context) => Login(),
+        MyRoutes.homeRoute:(context) => HomePage()
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -12,52 +13,56 @@ class Login extends StatelessWidget {
 
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login_image.png",fit: BoxFit.cover,),
-        SizedBox(
-          height: 20.0,
-          width: 20.0,
-        ),
-        // ignore: prefer_const_constructors
-        Text(
-          "Welcome",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-         SizedBox(
-          height: 20.0,
-          width: 20.0,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
-          child: Column(
-            children: [
-              TextFormField(
-          decoration: InputDecoration(
-            hintText: "Enter Username",
-            labelText: "UserName",
-          ),
-        ),
-        TextFormField(
-          obscureText:true,
-          decoration: InputDecoration(
-            hintText: "Enter PassWord",
-            labelText: "PassWord",
-          ),
-        ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.png",fit: BoxFit.cover,height: 200,),
           SizedBox(
-          height: 20.0,
-          width: 20.0,
-        ),
-        ElevatedButton(onPressed: (){print("Hi Codepur");}, child: Text("Login"),style: TextButton.styleFrom())
-        ],))
-
-
-        ],
-        )
+            height: 20.0,
+            width: 20.0,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+           SizedBox(
+            height: 20.0,
+            width: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+            decoration: InputDecoration(
+              hintText: "Enter Username",
+              labelText: "UserName",
+            ),
+          ),
+          TextFormField(
+            obscureText:true,
+            decoration: InputDecoration(
+              hintText: "Enter PassWord",
+              labelText: "PassWord",
+            ),
+          ),
+            SizedBox(
+            height: 20.0,
+            width: 20.0,
+          ),
+          ElevatedButton(onPressed: (){Navigator.pushNamed(context, MyRoutes.homeRoute);},style: TextButton.styleFrom(
+            minimumSize: Size(150,40),
+          ), child: Text("Login")),
+          
+          ],))
+      
+      
+          ],
+          ),
+      )
     );
   }
 }
